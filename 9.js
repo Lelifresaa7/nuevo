471568ed7002958cb8b4a483c5c11548c0c11a1f
Lelifresaa7 <peal070705mmcrrsa5@soycecytem.mx>
2024-04-10 00:18:30 -0600

    update

---
+++ 9.js
@@ -0,0 +1,6 @@
+let dv = document.querySelector("#vancho");
+let dv = document.querySelector("#valto");
+let dv = document.querySelector("#vcolor");
+let dv = document.querySelector("#vborder");
+let dv = document.querySelector("vbordercolor");
+let dv = document.querySelector("#tipob");
